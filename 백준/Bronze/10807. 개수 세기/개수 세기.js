383d93ae191a9069a02fa8a07b61253e03651d2d
Jvelop <51289286+ParkJongJoon7128@users.noreply.github.com>
2025-08-28 19:57:46 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const N = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+const v = Number(input[2]);
+let cnt = 0;
+
+for (let i = 0; i < N; i++) {
+  if (arr[i] === v) cnt++;
+}
+
+console.log(cnt);
