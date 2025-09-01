8c5a62b225a63ffb1f0b54b397f281746fa59285
Jvelop <51289286+ParkJongJoon7128@users.noreply.github.com>
2025-09-02 00:11:23 +0900

    [Bronze I] Title: 평균, Time: 92 ms, Memory: 9612 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.js
@@ -0,0 +1,17 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+
+let sum = 0;
+const max = Math.max(...arr);
+
+for (let i = 0; i < N; i++) {
+  sum += (arr[i] / max) * 100;
+}
+
+console.log(sum / N);
