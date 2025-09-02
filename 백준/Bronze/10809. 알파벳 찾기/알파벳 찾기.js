b2003af575ef2dbb9aee566d0181916d6a0dce3b
Jvelop <51289286+ParkJongJoon7128@users.noreply.github.com>
2025-09-02 22:21:53 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 108 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,33 @@
+const S = require("fs").readFileSync("/dev/stdin").toString().trim();
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const arr = alphabet.map((a) => S.indexOf(a));
+
+console.log(arr.join(" "));
