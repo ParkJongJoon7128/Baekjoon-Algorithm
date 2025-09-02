a1f0518564605576882a8826abd27207f3d90eb7
Jvelop <51289286+ParkJongJoon7128@users.noreply.github.com>
2025-09-02 21:27:27 +0900

    [Bronze IV] Title: 숫자의 합, Time: 96 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.js
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -9,8 +9,8 @@ const str = input[1].split("").map(Number);
 
 let sum = 0;
 
-for (let i = 1; i <= N; i++) {
-  sum += str[i - 1];
+for (let i = 0; i < N; i++) {
+  sum += str[i];
 }
 
 console.log(sum);
