4e299ea158c44bd4289d254be2a735ebd011e266
Jvelop <51289286+ParkJongJoon7128@users.noreply.github.com>
2025-09-11 21:20:12 +0900

    [Bronze II] Title: 대표값2, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.js
@@ -0,0 +1,24 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const arr = [];
+
+for (let i = 0; i < 5; i++) {
+  arr.push(Number(input[i]));
+}
+
+arr.sort((a, b) => a - b);
+let sum = 0;
+
+for (let j = 0; j < 5; j++) {
+  sum += arr[j];
+}
+
+const avg = Math.floor(sum / arr.length);
+const mid = arr[Math.floor(arr.length / 2)];
+
+console.log(avg);
+console.log(mid);
