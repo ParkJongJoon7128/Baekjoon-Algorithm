2e400b49eeea969c569e3b21ea16a3983448d38c
Jvelop <51289286+ParkJongJoon7128@users.noreply.github.com>
2025-09-11 23:05:55 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 3828 ms, Memory: 62488 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,26 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = Number(input[0]);
+
+const arr = [];
+
+for (let i = 1; i <= N; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+  arr.push([a, b]);
+}
+
+arr.sort((p1, p2) => {
+  if (p1[0] !== p2[0]) {
+    return p1[0] - p2[0];
+  } else {
+    return p1[1] - p2[1];
+  }
+});
+
+for (let a of arr) {
+  console.log(a.join(" "));
+}
