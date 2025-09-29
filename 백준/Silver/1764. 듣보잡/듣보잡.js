e4671cbd61e61370b225becefc60c9df9185ab80
Jvelop <51289286+ParkJongJoon7128@users.noreply.github.com>
2025-09-29 23:12:03 +0900

    [Silver IV] Title: 듣보잡, Time: 532 ms, Memory: 28900 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.js
@@ -0,0 +1,35 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+const arr = [];
+const arr2 = [];
+const result = [];
+let count = 0;
+
+for (let i = 1; i <= N; i++) {
+  arr.push(input[i]);
+}
+
+const set_arr = new Set(arr);
+
+for (let j = N + 1; j <= N + M; j++) {
+  arr2.push(input[j]);
+}
+
+for (let a of arr2) {
+  if (set_arr.has(a)) {
+    count++;
+    result.push(a);
+  }
+}
+
+result.sort();
+
+console.log(count);
+for (let r of result) {
+  console.log(r);
+}
