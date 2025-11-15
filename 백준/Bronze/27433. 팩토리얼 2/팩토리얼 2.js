3374989f35a08c1b9200d0d652b3a5f299610715
Jvelop <51289286+ParkJongJoon7128@users.noreply.github.com>
2025-11-15 16:46:20 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 92 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.js
@@ -0,0 +1,16 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let N = Number(input[0]);
+
+if (N === 0 || N === 1) {
+  console.log(1);
+} else {
+  for (let i = N - 1; i >= 2; i--) {
+    N *= i;
+  }
+  console.log(N);
+}
