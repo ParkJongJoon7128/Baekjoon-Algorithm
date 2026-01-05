c9e91a85432a1043a934c1c05624d78feca2f3ed
Jvelop <51289286+ParkJongJoon7128@users.noreply.github.com>
2026-01-06 00:23:19 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,19 @@
+function solution(n) {
+    let sum = 0;
+    
+    if(n % 2 !== 0){
+        for(let i = 1; i <= n; i++){
+            if(i % 2 !== 0){
+                sum += i;
+            }
+        }
+    }else{
+        for(let i = 1; i <= n; i++){
+            if(i % 2 === 0){
+                sum += i * i;
+            }
+        }
+    }
+    
+    return sum;
+}
